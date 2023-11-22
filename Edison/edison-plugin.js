d493b74780d59a66023a5baaa6b70c75dca1b363
maple <103380848+starguiders@users.noreply.github.com>
2023-11-22 03:45:28 -0600

    Add files via upload

---
+++ Edison/edison-plugin.js
@@ -0,0 +1,45 @@
+(function () {
+  var html = document.documentElement;
+  var themeControls = document.querySelector("[data-theme-controls]");
+  var toggle = themeControls.querySelector(".color-scheme-toggle");
+  var prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+  var setToggleLabel = function () {
+    let mode;
+
+    mode = !html.dataset.colorScheme
+      ? prefersDark.matches
+        ? "dark"
+        : "light"
+      : html.dataset.colorScheme;
+
+    toggle.setAttribute(
+      "title",
+      "Enable" + (mode === "dark" ? "light" : "dark") + "mode"
+    );
+  };
+
+  var setColorScheme = function (value) {
+    html.dataset.colorScheme = value;
+    localStorage.setItem("color-scheme", value);
+    setToggleLabel();
+  };
+
+  var init = function () {
+    setToggleLabel();
+  };
+
+  toggle.addEventListener("click", function () {
+    if (!html.dataset.colorScheme && prefersDark.matches) {
+      setColorScheme("light");
+      updateColorSchemeStatus("light");
+      return;
+    }
+
+    var scheme = html.dataset.colorScheme === "dark" ? "light" : "dark";
+
+    setColorScheme(scheme);
+  });
+
+  init();
+})();
